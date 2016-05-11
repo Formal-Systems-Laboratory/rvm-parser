4fcf2d389ef2a6a40c75e3b2729ce972ba8cb43e
xiaohe27 <xiaoguoyi27@gmail.com>
2016-05-11 00:09:06 -0500

    update

--- test/rvm/fsm/rvc/__RVC_Test_Monitor.c
+++ test/rvm/fsm/rvc/__RVC_Test_Monitor.c
@@ -16,10 +16,10 @@ void __RVC_Test_reset(void)
 Prop_0_RVC_state = 0;
 Prop_1_RVC_state = 0;
 }
-const static int Test_Prop_0_hasnext[]= {1, 1, 1, 3, };
-const static int Test_Prop_0_next[]= {2, 0, 2, 3, };
-const static int Test_Prop_1_hasnext[]= {1, 1, 1, 3, };
-const static int Test_Prop_1_next[]= {2, 0, 2, 3, };
+const static int Test_Prop_0_hasnext[]= {1, 11, 3};
+const static int Test_Prop_0_next[]= {2, 02, 3};
+const static int Test_Prop_1_hasnext[]= {1, 11, 3};
+const static int Test_Prop_1_next[]= {2, 02, 3};
 
 
 
@@ -86,6 +86,12 @@ if (Test_Prop_1_unsafe2) {
 {
 		int k;
 fprintf(stderr, "It's in state unsafe2.\n");
+	}
+}
+
+}
+
+.\n");
 	}
 }
 
