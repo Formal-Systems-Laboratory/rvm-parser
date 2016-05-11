71364b36221bfeb4fac13adbd250ca4d9a48a049
xiaohe27 <xiaoguoyi27@gmail.com>
2016-05-10 22:26:39 -0500

    re-structure the test

---
+++ test/rvm/fsm/rvc/__RVC_HasNext_Monitor.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+static int Prop_0_RVC_state = 0;
+
+int HasNext_Prop_0_start = 0;
+int HasNext_Prop_0_safe = 0;
+int HasNext_Prop_0_unsafe = 0;
+void __RVC_HasNext_reset(void)
+{
+Prop_0_RVC_state = 0;
+}
+const static int HasNext_hasnext[] = {1, 1, 1, 3, };
+const static int HasNext_next[] = {2, 0, 2, 3, };
+
+
+
+
+void __RVC_HasNext_hasnext(Iterator i)
+{
+
+{}
+Prop_0_RVC_state = HasNext_hasnext[Prop_0_RVC_state];
+start = Prop_0_RVC_state == 0
+safe = Prop_0_RVC_state == 1
+unsafe = Prop_0_RVC_state == 2
+if (HasNext_Prop_0_unsafe) {
+{
+fprintf(stderr, "next called without hasNext!\n");
+   }
+}
+
+}
+
+
+
+void __RVC_HasNext_next(Iterator i)
+{
+
+{}
+Prop_0_RVC_state = HasNext_next[Prop_0_RVC_state];
+start = Prop_0_RVC_state == 0
+safe = Prop_0_RVC_state == 1
+unsafe = Prop_0_RVC_state == 2
+if (HasNext_Prop_0_unsafe) {
+{
+fprintf(stderr, "next called without hasNext!\n");
+   }
+}
+
+}
+
---
+++ test/rvm/fsm/rvc/rv-monitor-output/__RVC_HasNext_Monitor.h
@@ -0,0 +1,10 @@
+#ifndef __RVC_HASNEXT_MONITOR_H
+#define __RVC_HASNEXT_MONITOR_H
+void
+__RVC_HasNext_reset(void);
+void
+__RVC_HasNext_next(Iterator i);
+void
+__RVC_HasNext_hasnext(Iterator i);
+
+#endif
