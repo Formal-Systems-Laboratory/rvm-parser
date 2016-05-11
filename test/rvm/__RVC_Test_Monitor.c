a1a11fefe60b79d1fb1a08d0fabe330b37c17b2d
xiaohe27 <xiaoguoyi27@gmail.com>
2016-05-10 20:37:47 -0500

    generate the .c code successfully!

---
+++ test/rvm/__RVC_Test_Monitor.c
@@ -0,0 +1,96 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+x = "foo"; 
+	class bar {
+		private int foo() { return 1; }
+		}
+
+   
+static int Prop_0_RVC_state = 0;
+
+static int Prop_1_RVC_state = 0;
+
+int Test_Prop_0_start = 0;
+int Test_Prop_0_safe = 0;
+int Test_Prop_0_unsafe = 0;
+int Test_Prop_1_start2 = 0;
+int Test_Prop_1_safe2 = 0;
+int Test_Prop_1_unsafe2 = 0;
+void __RVC_Test_reset(void)
+{
+Prop_0_RVC_state = 0;
+Prop_1_RVC_state = 0;
+}
+const static int Test_Prop_0_hasnext[]= {1, 1, 1, 3, };
+const static int Test_Prop_0_next[]= {2, 0, 2, 3, };
+const static int Test_Prop_1_hasnext[]= {1, 1, 1, 3, };
+const static int Test_Prop_1_next[]= {2, 0, 2, 3, };
+
+
+
+
+void __RVC_Test_hasnext(Iterator i)
+{
+
+{}
+Prop_0_RVC_state = Test_Prop_0_hasnext[Prop_0_RVC_state];
+start = Prop_0_RVC_state == 0
+safe = Prop_0_RVC_state == 1
+unsafe = Prop_0_RVC_state == 2
+Prop_1_RVC_state = Test_Prop_1_hasnext[Prop_1_RVC_state];
+start2 = Prop_1_RVC_state == 0
+safe2 = Prop_1_RVC_state == 1
+unsafe2 = Prop_1_RVC_state == 2
+if (Test_Prop_0_start){
+{
+		int j;
+	}
+}
+if (Test_Prop_0_unsafe){
+{
+	System.err.println("It is not safe");
+	}
+}
+if (Test_Prop_1_unsafe2){
+{
+		int k;
+	}
+}
+
+}
+
+
+
+void __RVC_Test_next(Iterator i)
+{
+
+{ int i = 0; {i--;} int j=i+ 2 ;
+{{{}}} }
+Prop_0_RVC_state = Test_Prop_0_next[Prop_0_RVC_state];
+start = Prop_0_RVC_state == 0
+safe = Prop_0_RVC_state == 1
+unsafe = Prop_0_RVC_state == 2
+Prop_1_RVC_state = Test_Prop_1_next[Prop_1_RVC_state];
+start2 = Prop_1_RVC_state == 0
+safe2 = Prop_1_RVC_state == 1
+unsafe2 = Prop_1_RVC_state == 2
+if (Test_Prop_0_start){
+{
+		int j;
+	}
+}
+if (Test_Prop_0_unsafe){
+{
+	System.err.println("It is not safe");
+	}
+}
+if (Test_Prop_1_unsafe2){
+{
+		int k;
+	}
+}
+
+}
+
+
